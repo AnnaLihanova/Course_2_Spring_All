a3f05cb98a2cf81162307428e7f985750ea07fe1
AnnaLihanova <anja.lihanova@gmail.com>
2022-04-29 16:21:04 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course_2_spring_all/Course2SpringAllApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course_2_spring_all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2SpringAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2SpringAllApplication.class, args);
+    }
+
+}
